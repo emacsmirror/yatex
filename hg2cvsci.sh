8de076af2e4ea8d1e6fcc883a9b818cf6603fdb2
Unknown <yuuji@gentei.org>
2009-10-15 23:11:31 +0900

    Comment more explanatory

--- hg2cvsci.sh
+++ hg2cvsci.sh
@@ -3,7 +3,9 @@
 # If new file is added by "hg add", don't forget to "cvs add" in
 # CVS working dir.
 #
-# This script should be called via incomming.hook.
+# This script should be called via incoming hook in .hg/hgrc as below.
+# [hooks]
+# incoming.cvsci = ./hg2cvsci.sh
 
 node=${HG_NODE:-tip}
 msg=`hg log -r $node --template '{desc}\n'`
